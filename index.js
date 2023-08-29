63c7b05d5332b0b85487cfa737fab76e3acaf292
Lucas Strangis <lucasstrangis1@gmail.com>
2023-08-29 14:23:40 -0300

    Proyecto Final

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const cambiarTemaButton = document.getElementById("cambiar-tema");
-    const mostrarDatosButton = document.getElementById("mostrar-datos");
-    const enviarDatosButton = document.getElementById("enviar-datos");
-    const formulario = document.querySelector("form");
-    const listaCarrito = document.getElementById("lista-carrito");
-    const totalCarrito = document.getElementById("total-carrito");
-    const vaciarCarritoButton = document.getElementById("vaciar-carrito");
+$(document).ready(() => {
+    const cambiarTemaButton = $("#cambiar-tema");
+    const mostrarDatosButton = $("#mostrar-datos");
+    const enviarDatosButton = $("#enviar-datos");
+    const formulario = $("form");
+    const listaCarrito = $("#lista-carrito");
+    const totalCarrito = $("#total-carrito");
+    const vaciarCarritoButton = $("#vaciar-carrito");
 
     let carrito = [];
 
-    cambiarTemaButton.addEventListener("click", () => {
-        document.body.classList.toggle("dark-theme");
+    cambiarTemaButton.click(() => {
+        $("body").toggleClass("dark-theme");
         actualizarCarrito();
     });
 
-    mostrarDatosButton.addEventListener("click", () => {
+    mostrarDatosButton.click(() => {
         const datosAlmacenados = JSON.parse(localStorage.getItem("datos"));
         if (datosAlmacenados) {
-            const datosDiv = document.createElement("div");
-            datosDiv.innerHTML = `<h3>Datos Almacenados</h3>
-                                  <p>Nombre: ${datosAlmacenados.nombre}</p>
-                                  <p>Apellido: ${datosAlmacenados.apellido}</p>
-                                  <p>Email: ${datosAlmacenados.email}</p>
-                                  <p>Mensaje: ${datosAlmacenados.mensaje}</p>`;
-            formulario.parentNode.insertBefore(datosDiv, formulario.nextSibling);
+            const datosDiv = $("<div>").html(`
+                <h3>Datos Almacenados</h3>
+                <p>Nombre: ${datosAlmacenados.nombre}</p>
+                <p>Apellido: ${datosAlmacenados.apellido}</p>
+                <p>Email: ${datosAlmacenados.email}</p>
+                <p>Mensaje: ${datosAlmacenados.mensaje}</p>
+            `);
+            formulario.after(datosDiv);
         }
     });
 
-    enviarDatosButton.addEventListener("click", (event) => {
+    enviarDatosButton.click((event) => {
         event.preventDefault();
-        const nombre = document.getElementById("nombre").value;
-        const apellido = document.getElementById("apellido").value;
-        const email = document.getElementById("email").value;
-        const mensaje = document.getElementById("mensaje").value;
+        const nombre = $("#nombre").val();
+        const apellido = $("#apellido").val();
+        const email = $("#email").val();
+        const mensaje = $("#mensaje").val();
 
         const datos = {
             nombre,
@@ -43,27 +44,46 @@ document.addEventListener("DOMContentLoaded", () => {
 
         localStorage.setItem("datos", JSON.stringify(datos));
 
-        const notificacionDiv = document.createElement("div");
-        notificacionDiv.textContent = "Datos almacenados con éxito";
-        notificacionDiv.style.color = "green";
-        formulario.parentNode.insertBefore(notificacionDiv, formulario.nextSibling);
-
-        formulario.reset();
+        const notificacionDiv = $("<div>")
+            .text("Datos almacenados con éxito")
+            .css("color", "green");
+        formulario.after(notificacionDiv);
+        formulario.trigger("reset");
     });
 
-    function actualizarCarrito() {
-        listaCarrito.innerHTML = "";
-        let total = 0;
+    fetch("productos.json")
+        .then(response => response.json())
+        .then(productos => {
+            const productosLista = $(".productos-lista");
+            productos.forEach(producto => {
+                const productoItem = $("<li>");
+                productoItem.html(`
+                    <img src="${producto.imagen}" alt="${producto.nombre}">
+                    <p>${producto.nombre}</p>
+                    <p>Precio: $${producto.precio.toFixed(2)}</p>
+                    <button class="agregar-carrito">Agregar al Carrito</button>
+                `);
+                productosLista.append(productoItem);
+            });
 
-        carrito.forEach((producto) => {
-            const productoItem = document.createElement("li");
-            productoItem.textContent = `${producto.nombre} - Precio: $${producto.precio.toFixed(2)}`;
-            listaCarrito.appendChild(productoItem);
-            total += producto.precio;
+            const agregarCarritoButtons = $(".agregar-carrito");
+            agregarCarritoButtons.click((event) => {
+                const productoContainer = $(event.target).closest("li");
+                const nombre = productoContainer.find("p:nth-child(2)").text();
+                const precioTexto = productoContainer.find("p:nth-child(3)").text();
+                const precio = parseFloat(precioTexto.slice(9));
+
+                agregarProductoAlCarrito(nombre, precio);
+            });
+        })
+        .catch(error => {
+            console.error("Error al cargar los productos:", error);
         });
 
-        totalCarrito.textContent = `Total: $${total.toFixed(2)}`;
-    }
+    vaciarCarritoButton.click(() => {
+        carrito = [];
+        actualizarCarrito();
+    });
 
     function agregarProductoAlCarrito(nombre, precio) {
         const producto = {
@@ -74,21 +94,16 @@ document.addEventListener("DOMContentLoaded", () => {
         actualizarCarrito();
     }
 
-    const agregarCarritoButtons = document.querySelectorAll(".agregar-carrito");
-    agregarCarritoButtons.forEach((button) => {
-        button.addEventListener("click", (event) => {
-            const productoContainer = event.target.closest("li");
-            const nombre = productoContainer.querySelector("p:nth-child(2)").textContent;
-            const precioTexto = productoContainer.querySelector("p:nth-child(3)").textContent;
-            const precio = parseFloat(precioTexto.slice(9));
+    function actualizarCarrito() {
+        listaCarrito.empty();
+        let total = 0;
 
-            agregarProductoAlCarrito(nombre, precio);
+        carrito.forEach(producto => {
+            const productoItem = $("<li>").text(`${producto.nombre} - Precio: $${producto.precio.toFixed(2)}`);
+            listaCarrito.append(productoItem);
+            total += producto.precio;
         });
-    });
 
-    vaciarCarritoButton.addEventListener("click", () => {
-        carrito = [];
-        actualizarCarrito();
-    });
+        totalCarrito.text(`Total: $${total.toFixed(2)}`);
+    }
 });
-
